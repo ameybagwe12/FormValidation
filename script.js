7ca2e80eda4d18759a0683b7789db45c1ea01244
ameybagwe12 <89726637+ameybagwe12@users.noreply.github.com>
2022-10-15 15:33:08 +0530

    Final Commit

--- script.js
+++ script.js
@@ -19,10 +19,12 @@ function checkUsername() {
     {
         formDiv[0].style.color = "red";
         formDiv[0].innerHTML = "Please enter username more than 5 characters !!";
+        alert("Please Retry")
     }
     else if(username.value.includes(" ")){
         formDiv[0].innerHTML = "Spaces are not allowed !!";
         formDiv[0].style.color = "red";
+        alert("Please Retry")
     }
     else{
         formDiv[0].innerHTML = "Looks Good !!";
@@ -38,10 +40,12 @@ function checkEmail() {
     {
         formDiv[1].style.color = "red";
         formDiv[1].innerHTML = "Please enter correct email address !!";
+        alert("Please Retry")
     }
     else if(email.value.includes(" ")){
         formDiv[1].innerHTML = "Spaces are not allowed !!";
         formDiv[1].style.color = "red";
+        alert("Please Retry")
     }
     else{
         formDiv[1].innerHTML = "Looks Good !!";
@@ -57,10 +61,12 @@ function checkPhone() {
     {
         formDiv[2].style.color = "red";
         formDiv[2].innerHTML = "Please enter correct phone number !!";
+        alert("Please Retry")
     }
     else if(phone.value.includes(" ")){
         formDiv[2].innerHTML = "Spaces are not allowed !!";
         formDiv[2].style.color = "red";
+        alert("Please Retry")
     }
     else{
         formDiv[2].innerHTML = "Looks Good !!";
@@ -78,6 +84,7 @@ function checkPass() {
     else if(pass.value.length < 8 || pass.value == "password"){
         formDiv[3].innerHTML = "Password is not strong !!";
         formDiv[3].style.color = "red";
+        alert("Please Retry")
     }   
     else if(pass.value == confirmPass.value)
     {
@@ -90,10 +97,12 @@ function checkPass() {
     else if(pass.value.includes(" ")){
         formDiv[3].innerHTML = "Spaces are not allowed !!";
         formDiv[3].style.color = "red";
+        alert("Please Retry")
     }
     else if(confirmPass.value.includes(" ")) {
         formDiv[4].innerHTML = "Spaces are not allowed !!";
         formDiv[4].style.color = "red";
+        alert("Please Retry")
     }
     else{
         formDiv[3].innerHTML = "Passwords did not matched !!";
